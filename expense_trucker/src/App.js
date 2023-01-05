5af592729367839a03b216bd86ab55635db04d89
Bekova03 <bekbolotovanazimm@gmail.com>
2023-01-05 12:14:59 +0600

    save

---
+++ expense_trucker/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Expense from './components/Expense';
+import { ExpenseItem } from './components/ExpenseItem'
+
+function App() {
+  const expense = [
+    {
+      id: 'el1',
+      title: 'Toilet Paper',
+      date: new Date(2023, 4, 12),
+      amount: 1.5
+    },
+    {
+      id: 'el2',
+      title: 'Car Insurance',
+      date: new Date(2023, 2, 20),
+      amount: 1.5
+    }
+  ]
+  return (
+    <div className="App">
+      {<Expense expense={expense}/>}
+      <ExpenseItem
+       title={expense[0].title} 
+       date={expense[0].date} 
+       amount={expense[0].amount}
+       />
+      <ExpenseItem
+       title={expense[1].title} 
+       date={expense[1].date} 
+       amount={expense[1].amount}
+       />
+    </div>
+  );
+}
+
+export default App;
---
+++ expense_trucker/src/components/Expense.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ExpenseItem } from './ExpenseItem'
+const Expense = (props) => {
+	return (
+		<div>
+			<ExpenseItem/>	
+			<ExpenseItem/>	
+			<ExpenseItem/>	
+		</div>
+	)
+}
+
+export default Expense
